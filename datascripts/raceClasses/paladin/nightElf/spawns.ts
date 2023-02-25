31501b1e153eb2ee8ed503fd986b351c6cd85e36
Arcfalt <me@arcfalt.com>
2023-02-25 18:57:17 +1000

    Updated night elf paladin trainers to new standards

--- datascripts/raceClasses/paladin/nightElf/spawns.ts
+++ datascripts/raceClasses/paladin/nightElf/spawns.ts
@@ -15,7 +15,7 @@ export const NE_PLD_TRAINER_SHADOWGLEN = buildNpc({
 
 	TrainerClassMask: ClassMask.PALADIN,
 	TrainerID: TrainerID.PaladinLow,
-	TrainerGossipClass: "Welcome, young one. Through our worship of Mother Moon, and observation of our new allies, we of the New Moon have begun to form the beginnings of a Paladin order. Have you come to train?",
+	TrainerGossipClass: "Welcome, young one. Through our worship of Mother Moon, and observation of our new allies, we of the New Moon have begun to form the beginnings of a Paladin order.  Have you come to train?",
 	TrainerGossipNotClass: "It appears you are not one to harness the light of the moon and balance of the blade together in harmony. Move along, young one.",
 
 	BakedNpc: "nePldTrainer00",
@@ -34,7 +34,7 @@ export const NE_PLD_TRAINER_DOLANAAR = buildNpc({
 
 	TrainerClassMask: ClassMask.PALADIN,
 	TrainerID: TrainerID.PaladinAlliance,
-	TrainerGossipClass: "Through our worship of Mother Moon, and observation of our new allies, we of the New Moon have begun to form our own Paladin order. Have you come to train?",
+	TrainerGossipClass: "Through our worship of Mother Moon, and observation of our new allies, we of the New Moon have begun to form our own Paladin order.  Have you come to train?",
 	TrainerGossipNotClass: "Elune guide you, $c.",
 
 	GuardGossipOrigin: 3564,
@@ -44,13 +44,14 @@ export const NE_PLD_TRAINER_DOLANAAR = buildNpc({
 
 	BakedNpc: "nePldTrainer01",
 	BakedNpcSound: NpcSoundIDs.NightElfFemalePriestess,
-	EquipMainHand: 5344,
+	EquipMainHand: 3319,
 	EquipOffHand: 876,
 });
 
 export const NE_PLD_TRAINER_DARNASSUS_LOWER = buildNpc({
 	Tag: 'ne-pld-trainer-darnassus-lower',
 	CopyFrom: 5491,
+	Level: 50,
 
 	Name: 'Fyliyell Farleaf',
 	Subname: 'Paladin Trainer',
@@ -59,22 +60,23 @@ export const NE_PLD_TRAINER_DARNASSUS_LOWER = buildNpc({
 
 	TrainerClassMask: ClassMask.PALADIN,
 	TrainerID: TrainerID.PaladinAlliance,
-	TrainerGossipClass: "Through our worship of Mother Moon, and observation of our new allies, we of the New Moon have begun to form our own Paladin order. Have you come to train?",
+	TrainerGossipClass: "Through our worship of Mother Moon, and observation of our new allies, we of the New Moon have begun to form our own Paladin order.  Have you come to train?",
 	TrainerGossipNotClass: "Elune guide you, $c.",
 	
 	GuardGossipOrigin: 2343,
 	GuardGossipOption: 'Paladin',
-	GuardGossipPoiName: 'Darnassus Paladin Trainer',
-	GuardGossipText: "Our new order of Paladins can be found residing within the Temple of the Moon. Seek out Fyliyell, she will guide you into this new style of training.",
+	GuardGossipPoiName: 'Darnassus Paladin Trainers',
+	GuardGossipText: "Our new order of Paladins can be found residing within the Temple of the Moon.  Seek out the Farleaf sisters, they will guide you into this new style of training.",
 
 	BakedNpc: "nePldTrainer02",
 	BakedNpcSound: NpcSoundIDs.NightElfFemalePriestess,
-	EquipMainHand: 1990,
+	EquipMainHand: 9490,
 });
 
 export const NE_PLD_TRAINER_DARNASSUS = buildNpc({
 	Tag: 'ne-pld-trainer-darnassus',
 	CopyFrom: 5492,
+	Level: 60,
 
 	Name: 'Ilyesana Silverstar',
 	Subname: 'Paladin Trainer',
@@ -83,12 +85,32 @@ export const NE_PLD_TRAINER_DARNASSUS = buildNpc({
 
 	TrainerClassMask: ClassMask.PALADIN,
 	TrainerID: TrainerID.PaladinAlliance,
-	TrainerGossipClass: "Through our worship of Mother Moon, and observation of our new allies, we of the New Moon have begun to form our own Paladin order. Have you come to train?",
+	TrainerGossipClass: "Through our worship of Mother Moon, and observation of our new allies, we of the New Moon have begun to form our own Paladin order.  Have you come to train?",
 	TrainerGossipNotClass: "Elune guide you, $c.",
 
 	BakedNpc: "nePldTrainer03",
 	BakedNpcSound: NpcSoundIDs.NightElfFemaleSentinel,
-	EquipMainHand: 16039,
+	EquipMainHand: 913,
+});
+
+export const NE_PLD_TRAINER_DARNASSUS_2 = buildNpc({
+	Tag: 'ne-pld-trainer-darnassus-2',
+	CopyFrom: 5492,
+	Level: 40,
+
+	Name: 'Ayanaya Farleaf',
+	Subname: 'Paladin Trainer',
+	Spawn: {map:1,x:9608.135742,y:2537.576416,z:1331.629395,o:5.601935},
+	Faction: FactionTemplateValues.DARNASSUS,
+
+	TrainerClassMask: ClassMask.PALADIN,
+	TrainerID: TrainerID.PaladinAlliance,
+	TrainerGossipClass: "Through our worship of Mother Moon, and observation of our new allies, we of the New Moon have begun to form our own Paladin order.  Have you come to train?",
+	TrainerGossipNotClass: "Elune guide you, $c.",
+
+	BakedNpc: "nePldTrainer04",
+	BakedNpcSound: NpcSoundIDs.NightElfFemaleSentinel,
+	EquipMainHand: 9490,
 });
 
 std.CreatureTemplates.load(35281).Spawns.getIndex(0).delete();
