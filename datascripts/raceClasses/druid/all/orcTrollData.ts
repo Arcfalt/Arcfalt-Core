47bea9a8a24797d65432300c1570b309b8bce1ff
Arcfalt <me@arcfalt.com>
2023-02-26 18:45:52 +1000

    Updated orc/troll druid/paladin trainers to updated lore standards

--- datascripts/raceClasses/druid/all/orcTrollData.ts
+++ datascripts/raceClasses/druid/all/orcTrollData.ts
@@ -2,24 +2,28 @@ import { std } from "wow/wotlk";
 import { ClassMask } from "wow/wotlk/std/Class/ClassRegistry";
 import { FactionTemplateValues } from "wow/wotlk/std/Faction/FactionTemplates";
 import { NpcSoundIDs, TrainerID } from "../../../commonIds";
+import { buildGossip } from "../../../gossipBuilder";
 import { buildNpc } from "../../../npcBuilder";
+
+const DEFAULT_GOSSIP = buildGossip({AddClassTrainer: {ClassMask: ClassMask.DRUID,
+	ClassText: "May de loa bring ya strength.  Channel de spirits o' nature, friend.",
+	NotClassText: "Ya'll need to find a different trainer, $c.  I cannot help ya.",
+}});
 
 buildNpc({
 	Tag: 'or-dru-trainer-valley',
 	CopyFrom: 3060,
 
-	Name: 'Hotah Pyrewinds',
+	Name: 'Jabul',
 	Subname: 'Druid Trainer',
 	Spawn: {map:1,x:-630.485291,y:-4213.135254,z:38.134029,o:0.980682},
 	Faction: FactionTemplateValues.ORGRIMMAR,
 
-	TrainerClassMask: ClassMask.DRUID,
 	TrainerID: TrainerID.DruidLow,
-	TrainerGossipClass: "Welcome, my $g brother : sister;.  If you are here to train, then I would be delighted to train you.$B$BWe are the preservers of the balance, now and forever, as Cenarius has taught us.  Never forget this.",
-	TrainerGossipNotClass: "We work to preserve the balance as Cenarius teaches, $c.  His is the light of a beacon that shows the way for us, and we must do everything in our power to preserve that light.",
+	Gossip: DEFAULT_GOSSIP,
 
 	BakedNpc: "orDruTrainer0",
-	BakedNpcSound: NpcSoundIDs.TaurenMaleElder,
+	BakedNpcSound: NpcSoundIDs.TrollMaleDark,
 	EquipMainHand: 5778,
 });
 
@@ -27,23 +31,21 @@ buildNpc({
 	Tag: 'or-dru-trainer-razor',
 	CopyFrom: 3064,
 
-	Name: 'Guhu Grasstalon',
+	Name: "Den'chulu",
 	Subname: 'Druid Trainer',
 	Spawn: {map:1,x:305.094727,y:-4839.462402,z:10.524177,o:1.376491},
 	Faction: FactionTemplateValues.ORGRIMMAR,
 
-	TrainerClassMask: ClassMask.DRUID,
 	TrainerID: TrainerID.Druid,
-	TrainerGossipClass: "Welcome, my $g brother : sister;.  If you are here to train, then I would be delighted to train you.$B$BWe are the preservers of the balance, now and forever, as Cenarius has taught us.  Never forget this.",
-	TrainerGossipNotClass: "We work to preserve the balance as Cenarius teaches, $c.  His is the light of a beacon that shows the way for us, and we must do everything in our power to preserve that light.",
+	Gossip: DEFAULT_GOSSIP,
 
 	GuardGossipOrigin: 3283,
 	GuardGossipOption: 'Druid',
-	GuardGossipPoiName: 'Guhu Grasstalon',
-	GuardGossipText: "Seek out Guhu Grasstalon in the barracks on the east side of Razor Hill. She's from Thunder Bluff and can probably help you.",
+	GuardGossipPoiName: "Den'chulu",
+	GuardGossipText: "Seek out Den'chulu in the barracks on the east side of Razor Hill.  I've seen her turn into a cat once...",
 
 	BakedNpc: "orDruTrainer1",
-	BakedNpcSound: NpcSoundIDs.TaurenFemaleStandard,
+	BakedNpcSound: NpcSoundIDs.TrollFemaleOld,
 	EquipMainHand: 15444,
 });
 
@@ -52,23 +54,58 @@ std.CreatureTemplates.load(3173).Spawns.getIndex(0).Position.set({map:1,x:309.05
 buildNpc({
 	Tag: 'or-dru-trainer-orgrimmar',
 	CopyFrom: 3033,
+	Level: 60,
 
-	Name: 'Isku Mistshout',
+	Name: 'Sesebi',
 	Subname: 'Druid Trainer',
-	Spawn: {map:1,x:1938.287109,y:-4211.719238,z:42.319805,o:3.065359},
+	Spawn: {map:1,x:1456.644897,y:-4220.769043,z:43.186043,o:0.050751},
 	Faction: FactionTemplateValues.ORGRIMMAR,
 
-	TrainerClassMask: ClassMask.DRUID,
 	TrainerID: TrainerID.Druid,
-	TrainerGossipClass: "Welcome, my $g brother : sister;.  If you are here to train, then I would be delighted to train you.$B$BWe are the preservers of the balance, now and forever, as Cenarius has taught us.  Never forget this.",
-	TrainerGossipNotClass: "We work to preserve the balance as Cenarius teaches, $c.  His is the light of a beacon that shows the way for us, and we must do everything in our power to preserve that light.",
+	Gossip: DEFAULT_GOSSIP,
 
 	GuardGossipOrigin: 1949,
 	GuardGossipOption: 'Druid',
-	GuardGossipPoiName: 'Orgrimmar Druid Trainer',
-	GuardGossipText: "Seek out Isku Mistshout in the entrance room of Thrall's Fortress in the Valley of Wisdom. He has come to discuss matters with our shaman, but I'm sure he can spare the time to assist you in your training.",
+	GuardGossipPoiName: 'Orgrimmar Druid Trainers',
+	GuardGossipText: "Look for Sesebi in the Darkbriar Lodge in the Valley of Spirits, up above and to the west of the Valley of Strength.",
 
 	BakedNpc: "orDruTrainer2",
-	BakedNpcSound: NpcSoundIDs.TaurenMaleShaman,
+	BakedNpcSound: NpcSoundIDs.TrollFemaleLaidBack,
+	EquipMainHand: 17743,
+});
+
+buildNpc({
+	Tag: 'or-dru-trainer-orgrimmar2',
+	CopyFrom: 3033,
+	Level: 50,
+
+	Name: "Keh'je",
+	Subname: 'Druid Trainer',
+	Spawn: {map:1,x:1477.814941,y:-4214.729980,z:43.185944,o:5.286729},
+	Faction: FactionTemplateValues.ORGRIMMAR,
+
+	TrainerID: TrainerID.Druid,
+	Gossip: DEFAULT_GOSSIP,
+
+	BakedNpc: "orDruTrainer3",
+	BakedNpcSound: NpcSoundIDs.TrollMaleShaman,
+	EquipMainHand: 17743,
+});
+
+buildNpc({
+	Tag: 'or-dru-trainer-orgrimmar3',
+	CopyFrom: 3033,
+	Level: 40,
+
+	Name: 'Fetho',
+	Subname: 'Druid Trainer',
+	Spawn: {map:1,x:1480.435181,y:-4218.779297,z:43.185944,o:2.184408},
+	Faction: FactionTemplateValues.ORGRIMMAR,
+
+	TrainerID: TrainerID.Druid,
+	Gossip: DEFAULT_GOSSIP,
+
+	BakedNpc: "orDruTrainer4",
+	BakedNpcSound: NpcSoundIDs.TrollFemaleStandard,
 	EquipMainHand: 17743,
 });
--- datascripts/raceClasses/paladin/all/orcTrollData.ts
+++ datascripts/raceClasses/paladin/all/orcTrollData.ts
@@ -2,24 +2,28 @@ import { std } from "wow/wotlk";
 import { ClassMask } from "wow/wotlk/std/Class/ClassRegistry";
 import { FactionTemplateValues } from "wow/wotlk/std/Faction/FactionTemplates";
 import { NpcSoundIDs, TrainerID } from "../../../commonIds";
+import { buildGossip } from "../../../gossipBuilder";
 import { buildNpc } from "../../../npcBuilder";
+
+const DEFAULT_GOSSIP = buildGossip({AddClassTrainer: {ClassMask: ClassMask.PALADIN,
+	ClassText: "May de' Light o' Lukou guide us, friend.  What can 'dis humble servant do for ya?",
+	NotClassText: "Light o' Lukou guide ya, $c.",
+}});
 
 buildNpc({
 	Tag: 'or-pld-trainer-valley',
 	CopyFrom: 15280,
 
-	Name: 'Muwgar',
+	Name: "Az'kea",
 	Subname: 'Paladin Trainer',
 	Spawn: {map:1,x:-639.194458,y:-4226.721680,z:38.134216,o:5.383162},
 	Faction: FactionTemplateValues.ORGRIMMAR,
 
-	TrainerClassMask: ClassMask.PALADIN,
 	TrainerID: TrainerID.PaladinLow,
-	TrainerGossipClass: "Welcome, $c. The so called light may have been unknown to us until recent years, but through our strength we can channel it just the same as any little human.",
-	TrainerGossipNotClass: "You'll have to find someone else to train you, $c.",
+	Gossip: DEFAULT_GOSSIP,
 
 	BakedNpc: "orPldTrainer0",
-	BakedNpcSound: NpcSoundIDs.OrcFemaleWarrior,
+	BakedNpcSound: NpcSoundIDs.TrollFemaleStandard,
 	EquipMainHand: 5779,
 });
 
@@ -27,25 +31,23 @@ buildNpc({
 	Tag: 'or-pld-trainer-razor',
 	CopyFrom: 16275,
 
-	Name: 'Gezirdrit',
+	Name: "Tzem'tei",
 	Subname: 'Paladin Trainer',
 	Spawn: {map:1,x:293.725220,y:-4830.029297,z:10.524028,o:0.616615},
 	Faction: FactionTemplateValues.ORGRIMMAR,
 
-	TrainerClassMask: ClassMask.PALADIN,
 	TrainerID: TrainerID.PaladinHorde,
-	TrainerGossipClass: "Welcome, $c. The so called light may have been unknown to us until recent years, but through our strength we can channel it just the same as any little human.",
-	TrainerGossipNotClass: "You'll have to find someone else to train you, $c.",
+	Gossip: DEFAULT_GOSSIP,
 
 	GuardGossipOrigin: 3283,
 	GuardGossipOption: 'Paladin',
-	GuardGossipPoiName: 'Gezirdrit',
-	GuardGossipText: "A Paladin? You'd be better staying true to being a warrior if you ask me, but you can find Gezirdrit in the barracks on the east side of Razor Hill.",
+	GuardGossipPoiName: "Tzem'tei",
+	GuardGossipText: "A Paladin?  That's like those Light of Lukou worshippers, right?  You can find Tzem'tei in the barracks on the east side of Razor Hill.",
 
 	BakedNpc: "orPldTrainer1",
-	BakedNpcSound: NpcSoundIDs.OrcFemaleShaman,
+	BakedNpcSound: NpcSoundIDs.TrollMaleStandard,
 	EquipMainHand: 14145,
-	EquipOffHand: 2446,
+	EquipOffHand: 2040,
 });
 
 std.CreatureTemplates.load(3706).Spawns.getIndex(0).Position.set({map:1,x:295.898987,y:-4833.096191,z:10.524028,o:0.616615});
@@ -54,23 +56,21 @@ buildNpc({
 	Tag: 'or-pld-trainer-orgrimmar1',
 	CopyFrom: 16681,
 
-	Name: 'Kovzarl Aridtooth',
+	Name: "Zar'sasay",
 	Subname: 'Paladin Trainer',
-	Spawn: {map:1,x:2028.221436,y:-4637.040039,z:29.781172,o:4.232431},
+	Spawn: {map:1,x:1457.811646,y:-4177.457520,z:44.290951,o:4.512091},
 	Faction: FactionTemplateValues.ORGRIMMAR,
 
-	TrainerClassMask: ClassMask.PALADIN,
 	TrainerID: TrainerID.PaladinHorde,
-	TrainerGossipClass: "Welcome, $c. The so called light may have been unknown to us until recent years, but through our strength we can channel it just the same as any little human.",
-	TrainerGossipNotClass: "You'll have to find someone else to train you, $c.",
+	Gossip: DEFAULT_GOSSIP,
 
 	GuardGossipOrigin: 1949,
 	GuardGossipOption: 'Paladin',
-	GuardGossipPoiName: 'Orgrimmar Paladin Trainer',
-	GuardGossipText: "Pah! You can find Kovzarl messing about with his precious light over in the little shack by the fishing pond in the Valley of Honor. He was once a feared warrior, but he's gone soft if you ask me.",
+	GuardGossipPoiName: 'Orgrimmar Paladin Trainers',
+	GuardGossipText: "Look for Zar'sasay and his little Light of Lukou group in the Spirits Lodge, in the Valley of Spirits.  They moved in with the priests there recently.",
 
 	BakedNpc: "orPldTrainer2",
-	BakedNpcSound: NpcSoundIDs.OrcMaleGuard,
+	BakedNpcSound: NpcSoundIDs.TrollMaleDark,
 	EquipMainHand: 1680,
 });
 
@@ -78,37 +78,33 @@ buildNpc({
 	Tag: 'or-pld-trainer-orgrimmar2',
 	CopyFrom: 16680,
 
-	Name: 'Omza Rapidwish',
+	Name: 'Zulre',
 	Subname: 'Paladin Trainer',
-	Spawn: {map:1,x:2033.455444,y:-4639.927734,z:29.781172,o:3.551083},
+	Spawn: {map:1,x:1459.427979,y:-4176.071777,z:61.560863,o:3.724069},
 	Faction: FactionTemplateValues.ORGRIMMAR,
 
-	TrainerClassMask: ClassMask.PALADIN,
 	TrainerID: TrainerID.PaladinHorde,
-	TrainerGossipClass: "Welcome, $c. The so called light may have been unknown to us until recent years, but through our strength we can channel it just the same as any little human.",
-	TrainerGossipNotClass: "You'll have to find someone else to train you, $c.",
+	Gossip: DEFAULT_GOSSIP,
 
 	BakedNpc: "orPldTrainer4",
-	BakedNpcSound: NpcSoundIDs.OrcFemaleStandard,
+	BakedNpcSound: NpcSoundIDs.TrollFemaleLaidBack,
 	EquipMainHand: 15228,
-	EquipOffHand: 4069,
+	EquipOffHand: 2040,
 });
 
 buildNpc({
 	Tag: 'or-pld-trainer-orgrimmar3',
 	CopyFrom: 16679,
 
-	Name: 'Ulugnest',
+	Name: 'Yuhize',
 	Subname: 'Paladin Trainer',
-	Spawn: {map:1,x:2025.279175,y:-4632.700195,z:29.781172,o:5.351614},
+	Spawn: {map:1,x:1449.482422,y:-4180.997070,z:61.560863,o:0.391362},
 	Faction: FactionTemplateValues.ORGRIMMAR,
 
-	TrainerClassMask: ClassMask.PALADIN,
 	TrainerID: TrainerID.PaladinHorde,
-	TrainerGossipClass: "Welcome, $c. The so called light may have been unknown to us until recent years, but through our strength we can channel it just the same as any little human.",
-	TrainerGossipNotClass: "You'll have to find someone else to train you, $c.",
+	Gossip: DEFAULT_GOSSIP,
 
 	BakedNpc: "orPldTrainer3",
-	BakedNpcSound: NpcSoundIDs.OrcMaleShady,
+	BakedNpcSound: NpcSoundIDs.TrollFemaleOld,
 	EquipMainHand: 18410,
 });
