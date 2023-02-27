2b790b8e38ea37dafac3424f9a114e5db5143d40
Arcfalt <me@arcfalt.com>
2023-02-27 19:30:15 +1000

    Added fix for undercity herbalism trainer not training

--- datascripts/classicFixes/trainerFixes.ts
+++ datascripts/classicFixes/trainerFixes.ts
@@ -6,3 +6,4 @@ std.SQL.gossip_menu_option.query({ MenuID: 6647, OptionType: 5, ActionMenuID: 79
 
 // todo - fix undercity herbalism trainer
 // GOSSIP_OPTION_TRAINER:: Player Sdfsde GUID Full: 0x0000000000000001 Type: Player Low: 1 requested wrong gossip menu: 7691 at Creature: Martha Alliestar (Entry: 4614)
+std.SQL.gossip_menu_option.query({ MenuID: 7691, OptionType: 5, ActionMenuID: 6647 }).ActionMenuID.set(0);
